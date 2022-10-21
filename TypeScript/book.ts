ba800d93532f90f80856237fdeb4c81a9c8f0952
alanheredero11@gmail.com <alanheredero11@gmail.com>
2022-10-21 16:16:18 +0200

    Upload Typecript Files

---
+++ TypeScript/book.ts
@@ -0,0 +1,14 @@
+class Book
+{
+    private title: string;
+    private nPages: number;
+    private isbn: string;
+    private author: string;
+    private editorial: string;
+
+    constructor()
+    {
+        this.title =
+    }
+
+}
