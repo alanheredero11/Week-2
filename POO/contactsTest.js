4813fa2a6c19b5848675b68d93d9f4b62f670ce5
alanheredero11@gmail.com <alanheredero11@gmail.com>
2022-10-21 16:31:07 +0200

    Correct CallBacks Classes

--- POO/contactsTest.js
+++ POO/contactsTest.js
@@ -2,15 +2,15 @@ let contacto_agenda = require("./contacts");
 
 let Persona1 = new contacto_agenda.contact;
 
-Persona1.contacto[0] = 
-{
+Persona1.contacto.push({  
+
     nombre: "Alan",
     apellido: "Heredero",
     altura: 1.75,
     peso: 80,
     yearOfBityh: 2000
 
-}
+})
 
 
 Persona1.printPersons()
