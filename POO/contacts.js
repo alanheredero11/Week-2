3ba3f20ec7b0d2be29e75bba0b73f3f24bb746a3
alanheredero11@gmail.com <alanheredero11@gmail.com>
2022-10-21 10:11:40 +0200

    Update Contacts Works

--- POO/contacts.js
+++ POO/contacts.js
@@ -4,32 +4,41 @@ class contact
 {
     constructor()
     {
-        [
-        this.contacto1 = new persona.Person(),
-        this.contacto2 = new persona.Person(),    
-        ]     
+        
+        this.contacto1 = [new persona.Person()]
+          
+           
     }
 
-    printAllHere()
+    printPersons()
     {
     console.log(this.contacto1);
-    console.log(this.contacto2)
     }
 }
 
-let Contacto1 = new contact();
+let Primer_Contacto = new contact();
 
-    Contacto1.contacto1.nombre = "Alberto";
-    Contacto1.contacto1.apellido = "Roberto";
-    Contacto1.contacto1.altura = 1.70;
-    Contacto1.contacto1.peso = 80;
-    Contacto1.contacto1.yearOfBirth = 1998;
-
-    Contacto1.contacto2.nombre = "Maria";
-    Contacto1.contacto2.apellido = "Moreno";
-    Contacto1.contacto2.altura = 1.60;
-    Contacto1.contacto2.peso = 50;
-    Contacto1.contacto2.yearOfBirth = 1999;
+Primer_Contacto.printPersons();
 
 
-Contacto1.printAllHere();
+
+
+
+
+
+
+
+
+
+
+    // Primer_Contacto.contacto1.nombre = "Alberto";
+    // Primer_Contacto.contacto1.apellido = "Roberto";
+    // Primer_Contacto.contacto1.altura = 1.70;
+    // Primer_Contacto.contacto1.peso = 80;
+    // Primer_Contacto.contacto1.yearOfBirth = 1998;
+
+    // Primer_Contacto.contacto2.nombre = "Maria";
+    // Primer_Contacto.contacto2.apellido = "Moreno";
+    // Primer_Contacto.contacto2.altura = 1.60;
+    // Primer_Contacto.contacto2.peso = 50;
+    // Primer_Contacto.contacto2.yearOfBirth = 1999;
